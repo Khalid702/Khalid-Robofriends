1652cd28f3e33c59fde25d90a8200c22f10d0b98
Khalid Elmi <elmikhalid12@gmail.com>
2020-09-12 11:48:54 +0300

    app created using react

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sasha Blouse ',
+    username: 'Sasha',
+    email: 'Sasha12@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Eren Yeager',
+    username: 'Eren',
+    email: 'erenyeager@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Mikasa Ackerman',
+    username: 'Mikasa',
+    email: 'Mikasa12@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Levi Ackerman',
+    username: 'Levi',
+    email: 'Levi12@gmail'
+  },
+  {
+    id: 5,
+    name: 'Armin Arlet',
+    username: 'Armin',
+    email: 'Armin12@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Hange Zoe',
+    username: 'Hange',
+    email: 'Hange@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Reiner Braun',
+    username: 'Reiner',
+    email: 'Reiner12@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Erwin Smith',
+    username: 'Danchou',
+    email: 'erwin12@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Jean Kirstein',
+    username: 'Jean',
+    email: 'jean12@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Bertolt Hoover',
+    username: 'Bertolt',
+    email: 'Bertolot12@gmail.com'
+  }
+];
